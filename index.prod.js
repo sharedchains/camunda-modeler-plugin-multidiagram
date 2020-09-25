45d076c2fe66585d937757d0b31706e2397510c6
Luca Bonora <luca.bonora@sharedchains.com>
2020-09-25 14:59:34 +0200

    Use webpack to create a zip plugin package easy to release

---
+++ index.prod.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  name: 'Multi-diagram model Plug-in',
+  script: './client/client.js',
+  style: './client/style.css'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -67,12 +68,26 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   plugins: [
     new CopyPlugin({
-        patterns: [
-            {
-                from : 'client/style.css',
-                to : './'
-            }
-        ]
+      patterns: [
+        {
+          from: path.resolve(__dirname, './client/style.css'),
+          to: path.resolve(__dirname, './dist/')
+        },
+        {
+          from: path.resolve(__dirname, './index.prod.js'),
+          to: path.resolve(__dirname, './dist/index.js')
+        }
+      ]
+    }),
+    new ZipPlugin({
+      filename: 'camunda-modeler-plugin-multidiagram-' + process.env.npm_package_version + '.zip',
+      pathPrefix: 'camunda-modeler-plugin-multidiagram/',
+      pathMapper: function(assetPath) {
+        if (assetPath.startsWith('client') || assetPath.startsWith('style')) {
+          return path.join(path.dirname(assetPath), 'client', path.basename(assetPath));
+        }
+        return assetPath;
+      }
     })
   ]
 };
